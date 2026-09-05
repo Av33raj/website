ac37f1dc5c74b02a5bde8cb2a156b128dfb3204e
Av33raj <aveerajchahal@gmail.com>
2026-09-05 17:17:02 +0100

    added new projects to the bog page

--- posts.py
+++ posts.py
@@ -10,5 +10,20 @@ posts = [
         "title": "OpenCV Hand Tracking",
         "summary": "Combining OpenCV and MediaPipe for hand landmark detection.",
         "content": "Full write-up of the project goes here..."
+    },
+
+    {
+        "slug": "Brute-Force Detector",
+        "title": "SSH/Auth Log Brute-Force Detector",
+        "summary": "Distinguishing brute-Force attacks based around the number of log in attempts.",
+        "content": "full right up goes here",
+
+    },
+
+    { 
+        "slug":"Portfolio website",
+        "title": "Html/css Portfilio Website",
+        "summary": "I built a built a protfolio website based arount the projects I have built and skills I am developing.",
+        "content": "write up goes here",
     }
 ]
